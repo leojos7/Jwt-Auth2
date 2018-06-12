b417fdef840ee91883a1be35d84cb1168dec1f91
Jose <leo.jose@siemens.com>
2018-06-12 18:10:02 +0530

    moving pacakges to exact ones

---
+++ src/main/java/com/siemens/mindsphere/apps/module/login/security/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.siemens.mindsphere.apps.module.login.security.repository;
+
+import com.siemens.mindsphere.apps.module.login.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+
+public interface UserRepository extends PagingAndSortingRepository<User, String> {
+
+    @Query("SELECT u FROM User u WHERE LOWER(u.username) = LOWER(:username)")
+    User findByUsernameCaseInsensitive(@Param("username") String username);
+
+}
